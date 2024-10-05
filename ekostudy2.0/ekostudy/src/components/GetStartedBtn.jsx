d4912ff63342b9402754dad4524bfd8b04ce61be
CodeGallantX <johnayobami77@proton.me>
2024-10-05 21:09:34 +0100

    ..

--- ekostudy2.0/ekostudy/src/components/GetStartedBtn.jsx
+++ ekostudy2.0/ekostudy/src/components/GetStartedBtn.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <button onClick="window.location(signin)" className="mt-4 lg:mt-0 text-lg xl:text-xl font-bold px-8 py-4 bg-[#FFCA0D] leading-none hover:bg-[#92B76D] transition-all duration-300 ease-in-out">
+    <button onClick={window.location.href = '/signup'} className="mt-4 lg:mt-0 text-lg xl:text-xl font-bold px-8 py-4 bg-[#FFCA0D] leading-none hover:bg-[#92B76D] transition-all duration-300 ease-in-out">
         GET STARTED
     </button>
   )
