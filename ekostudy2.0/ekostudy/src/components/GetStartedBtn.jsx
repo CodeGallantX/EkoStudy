f646ec08f07a727274de70e9f7c3451bbabed9d2
CodeGallantX <johnayobami77@proton.me>
2024-10-05 21:18:00 +0100

    updates

--- ekostudy2.0/ekostudy/src/components/GetStartedBtn.jsx
+++ ekostudy2.0/ekostudy/src/components/GetStartedBtn.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const App = () => {
   return (
-    <button onClick={window.open('/signup')} className="mt-4 lg:mt-0 text-lg xl:text-xl font-bold px-8 py-4 bg-[#FFCA0D] leading-none hover:bg-[#92B76D] transition-all duration-300 ease-in-out">
+    <Link to={'/signup'}>
+      <button className="mt-4 lg:mt-0 text-lg xl:text-xl font-bold px-8 py-4 bg-[#FFCA0D] leading-none hover:bg-[#92B76D] transition-all duration-300 ease-in-out">
         GET STARTED
-    </button>
+      </button>
+    </Link>
   )
 }
 
